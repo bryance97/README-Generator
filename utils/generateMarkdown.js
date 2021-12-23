0c3886a531ea32fad63d17caeda92c4affc9f398
bryance97 <bryancifuentesescobar@gmail.com>
2021-12-23 18:42:34 -0500

    Adding files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,108 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+if (license == 'APACHE 2.0') {
+  return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+} else if (license == 'GPL 3.0') {
+  return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+} else if (license == 'APACHE 2.0') {
+  return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+} else if (license == 'BSD 3') {
+  return "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)";
+} else {
+  return " ";
+}
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {function renderLicenseLink(license) {
+  if (license== 'MIT') {
+    return '(https://opensource.org/licenses/MIT)';
+  }
+  else if (license== 'GPL 3.0') {
+    return '(https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  else if (license== 'APACHE 2.0') {
+    return '(https://opensource.org/licenses/Apache-2.0)';
+  }
+  else if (license== 'BSD 3') {
+    return '(https://www.boost.org/LICENSE_1_0.txt)';
+  }
+  else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
 
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+  
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  }. You can find more of my work at [${data.github}](https://github.com/${
+    data.github
+  }/).
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+}}
